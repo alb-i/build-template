d4559d211c5b323bf2d38dd966e508a5de291896
Immanuel Albrecht <mail@immanuel-albrecht.de>
2024-02-17 16:15:29 +0100

    ...

--- macos/arm64/nix/flake.nix
+++ macos/arm64/nix/flake.nix
@@ -1,17 +1,29 @@
 # Macos base dev environment
 
 {
+    description = "General build environment";
+
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs";
+
+        sbt.url = "github:zaninime/sbt-derivation";
+        bt.inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    outputs = {self, nixpkgs}: {
+
+
+    outputs = { 
+        self, 
+        nixpkgs, 
+        sbt,
+        }: {
         packages."aarch64-darwin".default = let 
                 pkgs = nixpkgs.legacyPackages."aarch64-darwin";
         in pkgs.buildEnv {
             name = "home-packages";
             paths = with pkgs; [
                 git
+                sbt
             ];
         };
     };
